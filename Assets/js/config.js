43c33e5ee0dcff7c44d2a932e9a9f9fb95ca98e2
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-13 20:27:49 -0700

    add searched city to archive list

---
+++ Assets/js/config.js
@@ -0,0 +1,4 @@
+
+const config = {
+    APIKey : 'd085326e9285b26166dacc06e92c94ca'
+}
--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,45 +1,63 @@
-const searchBtn = document.getElementById("search-button");
+// grab API Key from config and Search btn from HTML
+const key = config.APIKey;
+const searchBtn = document.getElementById("search-btn");
 
-searchBtn.addEventListener("click", () => {
-  const cityInput = document.getElementById("city-input").value;
-  // check for empty input
-  getLatLon(cityInput);
-});
+// add city to archive (button list & localstorage)
+const addToArchive = (city) => {
+  const cityArchive = document.getElementById("city-archive");
+  cityArchive.innerHTML += `<button class="city-btn">${city}</button>`;
+};
 
-function getLatLon(city) {
-
-  const APIKey = "d085326e9285b26166dacc06e92c94ca";
-
+// get lat/lon based on city name
+const getLatLon = (city) => {
   let queryURL =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
-      city +
-      "&limit=1&appid=" +
-      APIKey;  
-    // "http://api.openweathermap.org/data/2.5/weather?q=" +
-    // city +
-    // "&appid=" +
-    // APIKey +
-    // "&units=imperial";
+    city +
+    "&limit=1&appid=" +
+    key;
 
-  // insert error for typing city that doesn't exist
+  // todo: insert error for typing city that doesn't exist
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((data) => {
+      let lat = data[0].lat;
+      let lon = data[0].lon;
+      getWeather(city, lat, lon);
+    });
+};
+
+const getWeather = (city, lat, lon) => {
+  let queryURL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=minutely,hourly,alerts&appid=" +
+    key;
+
   fetch(queryURL)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      // let selectedCity = document.getElementById("selected-city");
-      // let temp = document.getElementById("temp");
-      // let wind = document.getElementById("wind");
-      // let humidity = document.getElementById("humidity");
-      //   let uv = document.getElementById("uv");
 
-      // selectedCity.innerHTML += data.name;
-      // temp.innerHTML += data.main.temp;
-      // wind.innerHTML += data.wind.speed;
-      // humidity.innerHTML += data.main.humidity;
+      let selectedCity = document.getElementById("selected-city");
+      let temp = document.getElementById("temp");
+      let wind = document.getElementById("wind");
+      let humidity = document.getElementById("humidity");
+      let uvi = document.getElementById("uvi");
 
-      let lat = data.lat;
-      let lon = data.lon;
-      console.log(lat);
-      //   uv.innerHTML += data.main.uv;
+      selectedCity.innerHTML = city;
+      temp.innerHTML = data.current.temp;
+      wind.innerHTML = data.current.wind_speed;
+      humidity.innerHTML = data.current.humidity;
+      uvi.innerHTML = data.current.uvi;
     });
 };
+
+// upon search btn click, get lat/lon and add city to archive
+searchBtn.addEventListener("click", () => {
+  const city = document.getElementById("city-input").value;
+  // check for empty input
+  getLatLon(city);
+  addToArchive(city);
+});
